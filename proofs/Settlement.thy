b73fef7e9cdfbb159893e9137943c1fca4e015b1
Yoichi Hirai <yoichi@brainbot.com>
2019-01-30 14:33:36 +0100

    Fix equations numbers to match the spec

--- proofs/Settlement.thy
+++ proofs/Settlement.thy
@@ -19,7 +19,7 @@ text "TODO:
 type_synonym impl_number = "int option"
 
 definition valid :: "int \<Rightarrow> bool"
-  where "valid a = (0 \<le> a \<and> a < 32)"
+  where "valid a = (0 \<le> a \<and> a < 2 ^ 256)"
 
 definition chop :: "int \<Rightarrow> impl_number"
   where
@@ -107,50 +107,45 @@ definition spec_s2 :: "int \<Rightarrow> int \<Rightarrow> int \<Rightarrow> int
 
 lemma s1_correct :
 "
-valid T1 \<Longrightarrow>
-valid T2 \<Longrightarrow>
-valid L1 \<Longrightarrow>
-valid L2 \<Longrightarrow>
-valid D1 \<Longrightarrow>
-valid D2 \<Longrightarrow>
+valid T1 \<Longrightarrow> (* because it's uint256 *)
+valid T2 \<Longrightarrow> (* because it's uint256 *)
+valid L1 \<Longrightarrow> (* because it's uint256 *)
+valid L2 \<Longrightarrow> (* because it's uint256 *)
+valid D1 \<Longrightarrow> (* because it's uint256 *)
+valid D2 \<Longrightarrow> (* because it's uint256 *)
+valid W1 \<Longrightarrow> (* because it's uint256 *)
+valid W2 \<Longrightarrow> (* because it's uint256 *)
+valid (T1 + L1) \<Longrightarrow> (* (11 R) *)
+valid (T2 + L2) \<Longrightarrow> (* (11 R) *)
 valid (D1 + D2) \<Longrightarrow> (* (12) *)
-valid W1 \<Longrightarrow>
-valid W2 \<Longrightarrow>
-valid (T1 + L1) \<Longrightarrow> (* 10 *)
-valid (T2 + L2) \<Longrightarrow> (* 10 *)
-L1 <= D1 - W1 + T2 - T1 \<Longrightarrow> (* (5) *)
-D1 - W1 + T2 - T1 - L1 \<le> D1 + D2 - W1 - W2 \<Longrightarrow> (* (5) *)
--(D1 - W1) <= T2 + L2 - T1 - L1 \<Longrightarrow> (* (7) *)
-T2 + L2 - T1 - L1 <= D2 - W2 \<Longrightarrow> (* (8) *)
+D1 - W1 + T2 - T1 - L1 \<ge> 0 \<Longrightarrow>  (* (5 R) *)
+D1 - W1 + T2 - T1 - L1 \<le> D1 + D2 - W1 - W2 \<Longrightarrow> (* (5 R) *)
+-(D1 - W1) <= T2 + L2 - T1 - L1 \<and> T2 + L2 - T1 - L1 <= D2 - W2 \<Longrightarrow> (* (7 R) *)
 T2 + L2 \<ge> T1 + L1 \<Longrightarrow>
 S1 T1 L1 T2 L2 D1 W1 D2 W2 = (Some (spec_s1 T1 T2 D1 W1 L1))"
-  apply(auto simp add: valid_def spec_s1_def S1_def RmaxP1_def RmaxP1_pre_def TAD_def chop_def
- TLmax2_def SL2_def)
-   apply(simp add: TLmax1_def chop_def valid_def )
-  apply(auto)
-  done
+  by(auto simp add: valid_def spec_s1_def S1_def RmaxP1_def RmaxP1_pre_def TAD_def chop_def
+ TLmax2_def SL2_def TLmax1_def chop_def )
 
 
 
 lemma s2_correct :
 "
-valid T1 \<Longrightarrow>
-valid T2 \<Longrightarrow>
-valid L1 \<Longrightarrow>
-valid L2 \<Longrightarrow>
-valid D1 \<Longrightarrow>
-valid D2 \<Longrightarrow>
-valid W1 \<Longrightarrow>
-valid W2 \<Longrightarrow>
+valid T1 \<Longrightarrow> (* because it's uint256 *)
+valid T2 \<Longrightarrow> (* because it's uint256 *)
+valid L1 \<Longrightarrow> (* because it's uint256 *)
+valid L2 \<Longrightarrow> (* because it's uint256 *)
+valid D1 \<Longrightarrow> (* because it's uint256 *)
+valid D2 \<Longrightarrow> (* because it's uint256 *)
+valid W1 \<Longrightarrow> (* because it's uint256 *)
+valid W2 \<Longrightarrow> (* because it's uint256 *)
 valid (T1 + L1) \<Longrightarrow> (* (11 R) *)
 valid (T2 + L2) \<Longrightarrow> (* (11 R) *)
-L1 <= D1 - W1 + T2 - T1 \<Longrightarrow> (* (5 R) *)
+valid (D1 + D2) \<Longrightarrow> (* (12) *)
 D1 - W1 + T2 - T1 - L1 \<ge> 0 \<Longrightarrow>  (* (5 R) *)
 D2 - W2 + T1 - T2 - L2 \<ge> 0 \<Longrightarrow>  (* something similar to (5 R) but not documented in the spec *)
 D1 - W1 + T2 - T1 - L1 \<le> D1 + D2 - W1 - W2 \<Longrightarrow> (* (5 R) *)
 D2 - W2 + T1 - T2 - L2 \<le> D1 + D2 - W1 - W2 \<Longrightarrow> (* something similar to (5 R) but not documented in the spec *)
--(D1 - W1) <= T2 + L2 - T1 - L1 \<Longrightarrow> (* (7 R) *)
-T2 + L2 - T1 - L1 <= D2 - W2 \<Longrightarrow> (* (7 R) *)
+-(D1 - W1) <= T2 + L2 - T1 - L1 \<and> T2 + L2 - T1 - L1 <= D2 - W2 \<Longrightarrow> (* (7 R) *)
 T2 + L2 \<ge> T1 + L1 \<Longrightarrow>
 S2 T1 L1 T2 L2 D1 W1 D2 W2 = (Some (spec_s2 T1 T2 D2 W2 L2))"
   apply(auto simp add: valid_def spec_s2_def S2_def RmaxP2_def SL1_def TLmax1_def spec_s1_def S1_def RmaxP1_def RmaxP1_pre_def TAD_def chop_def
