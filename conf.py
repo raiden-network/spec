99988aae7473519929605a1b82bfcfeab4450b5d
Karl Bartel <karl42@gmail.com>
2022-08-11 17:22:17 +0200

    Update sphinx, remove soliditydomain
    The soliditydomain is not maintained and does not work with recent
    sphinx versions. Instead of autodetecting the methods, I just included
    the result directly. We only used the soliditydomain in a single case.

--- conf.py
+++ conf.py
@@ -39,7 +39,6 @@ release = '0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinxcontrib.soliditydomain',
     'matplotlib.sphinxext.plot_directive',
     'sphinxcontrib.mermaid',
 ]
@@ -65,7 +64,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
